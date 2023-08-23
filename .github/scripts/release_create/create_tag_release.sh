93210d2cf76cb788340a7e6a0c413a5ac03e5430
Humair Khan <HumairAK@users.noreply.github.com>
2023-08-23 13:36:20 -0400

    Move scripts to separate dir.
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

---
+++ .github/scripts/release_create/create_tag_release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo "Create a tag release for ${TARGET_VERSION_TAG} in ${REPOSITORY}"
+
+gh release create ${TARGET_VERSION_TAG} --target ${RELEASE_BRANCH} --generate-notes --notes-start-tag ${PREVIOUS_VERSION_TAG}
+
+cat <<EOF >> /tmp/release-notes.md
+
+This is a release comprising of multiple repos:
+* DSP component for ${TARGET_VERSION_TAG} can be found [here](https://github.com/opendatahub-io/data-science-pipelines/releases/tag/${TARGET_VERSION_TAG})
+* DSPO component for ${TARGET_VERSION_TAG} can be found [here](https://github.com/opendatahub-io/data-science-pipelines-operator/releases/tag/${TARGET_VERSION_TAG})
+
+Version Table for components can be found [here](https://github.com/opendatahub-io/data-science-pipelines-operator/blob/main/docs/release/compatibility.md)
+EOF
+
+echo "$(gh release view ${TARGET_VERSION_TAG} --json body --jq .body)" >> /tmp/release-notes.md
+
+echo "Release notes to be created:"
+cat /tmp/release-notes.md
+
+gh release edit ${TARGET_VERSION_TAG} --notes-file /tmp/release-notes.md
+rm /tmp/release-notes.md
